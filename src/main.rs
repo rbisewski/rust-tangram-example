1456ac47763995a25b9f52216c3c14da36bc1a6a
Robert Bisewski <contact@ibiscybernetics.com>
2021-12-04 16:52:52 -0600

    Read in csv user data and make some predictions

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,32 @@
-use tangram::PredictOutput;
+use std::env;
 
-struct HeartDisease {
-    age: f32,
-    gender: String,
-    chest_pain: String,
-    resting_blood_pressure: f32,
-    cholesterol: f32,
-    fasting_blood_sugar_greater_than_120: String,
-    resting_ecg_result: String,
-    exercise_max_heart_rate: f32,
-    exercise_induced_angina: String,
-    exercise_st_depression: f32,
-    exercise_st_slope: String,
-    fluoroscopy_vessels_colored: f32,
-    thallium_stress_test: String,
-}
+mod heartdisease;
+use heartdisease::{convert_to_records, predict};
+use tangram::{BinaryClassificationPredictOutput};
 
-fn predict(model: tangram::Model, entry: HeartDisease) -> PredictOutput {
-    let input = tangram::predict_input! {
-        "age": entry.age,
-        "gender": entry.gender,
-        "chest_pain": entry.chest_pain,
-        "resting_blood_pressure": entry.resting_blood_pressure,
-        "cholesterol": entry.cholesterol,
-        "fasting_blood_sugar_greater_than_120": entry.fasting_blood_sugar_greater_than_120,
-        "resting_ecg_result": entry.resting_ecg_result,
-        "exercise_max_heart_rate": entry.exercise_max_heart_rate,
-        "exercise_induced_angina": entry.exercise_induced_angina,
-        "exercise_st_depression": entry.exercise_st_depression,
-        "exercise_st_slope": entry.exercise_st_slope,
-        "fluoroscopy_vessels_colored": entry.fluoroscopy_vessels_colored,
-        "thallium_stress_test": entry.thallium_stress_test,
-    };
-
-    // make a single prediction
-    model.predict_one(input, None)
-}
+use crate::heartdisease::{HeartDiseaseWithDiagnosis, set_diagnosis};
 
 fn main() {
+
+    let args: Vec<String> = env::args().collect();
+    let flag: &String;
+    let filename: &String;
+
+    match args.len() {
+        3 => {
+            flag = &args[1];
+            filename = &args[2];
+        },
+        _ => {
+            println!("Usage: ./rust-tangram-example --input /path/to/new_data.csv");
+            return;
+        }
+    }
+
+    if flag.is_empty() || filename.is_empty() || flag != "--input" {
+        return;
+    }
+
     // load model
 	let resulting_model = tangram::Model::from_path(
         "heart_disease_training_data.tangram", 
@@ -50,23 +39,39 @@ fn main() {
     };
 
     // set an example heart disease entry
-    let entry = HeartDisease{
-        age: 63.0,
-        gender: "male".to_string(),
-        chest_pain: "typical angina".to_string(),
-        resting_blood_pressure: 145.0,
-        cholesterol: 233.0,
-        fasting_blood_sugar_greater_than_120: "true".to_string(),
-        resting_ecg_result: "probable or definite left ventricular hypertrophy".to_string(),
-        exercise_max_heart_rate: 150.0,
-        exercise_induced_angina: "no".to_string(),
-        exercise_st_depression: 2.3,
-        exercise_st_slope: "downsloping".to_string(),
-        fluoroscopy_vessels_colored: 0.0,
-        thallium_stress_test: "fixed defect".to_string(),
-    };
+    let entry = heartdisease::new(
+        63.0,
+        "male".to_string(),
+        "typical angina".to_string(),
+        145.0,
+        233.0,
+        "true".to_string(),
+        "probable or definite left ventricular hypertrophy".to_string(),
+        150.0,
+        "no".to_string(),
+        2.3,
+        "downsloping".to_string(),
+        0.0,
+        "fixed defect".to_string(),
+    );
+    let output = predict(&model, &entry);
+    println!("Single prediction\n{:?}", output);
 
-    let output = predict(model, entry);
+    let records = convert_to_records(filename.to_string());
+    let mut records_with_predictions: Vec<HeartDiseaseWithDiagnosis> = vec!();
 
-    println!("Output: {:?}", output);
+    for r in records {
+        let prediction_output = predict(&model, &r);
+        let binary_output: BinaryClassificationPredictOutput = BinaryClassificationPredictOutput::from(prediction_output);
+
+        let record_with_diagnosis = set_diagnosis(
+            &r,
+            binary_output.class_name,
+            binary_output.probability,
+        );
+
+        records_with_predictions.push(record_with_diagnosis);
+    }
+
+    println!("Multiple predictions\n{:#?}", records_with_predictions);
 }
